8ef288bf72cc6bc49c727a699b7ab491465eee2a
turkishcydia <82547225+turkishcydia@users.noreply.github.com>
2021-06-08 02:36:58 +0300

    Update muzaffer_cinar.js

--- muzaffer_cinar.js
+++ muzaffer_cinar.js
@@ -1,1 +1,18 @@
-lslsl
+// KURULUM
+var c = document.createElement("canvas");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+document.body.appendChild(c);
+var ctx = c.getContext("2d");
+
+
+
+// TANIMLAMA
+var bgcolor ="#ff4301";
+var forecolor = "#4a3f35";
+var linecolor = "#2f2519";
+var linewidth = 5;
+
+
+ctx.fillStyle = bgcolor;
+ctx.fillRect(0,0,c.width,c.height);
