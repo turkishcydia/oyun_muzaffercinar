d4a6d89e8794576bdae83c6416c0f83d82d18d19
turkishcydia <82547225+turkishcydia@users.noreply.github.com>
2021-06-18 14:05:00 +0300

    Update muz.js

--- muz.js
+++ muz.js
@@ -2,7 +2,6 @@
 
 //selecting all required elements
 const selectBox = document.querySelector(".select-box"),
-butdate = document.querySelector(".butdate"),
 selectBtnX = selectBox.querySelector(".options .playerX"),
 selectBtnO = selectBox.querySelector(".options .playerO"),
 playBoard = document.querySelector(".play-board"),
@@ -21,7 +20,6 @@ window.onload = ()=>{ //once window loaded
 selectBtnX.onclick = ()=>{
     selectBox.classList.add("hide"); //hide select box
     playBoard.classList.add("show"); //show the playboard section
-    butdate.classList.add("hide);
 }
 
 selectBtnO.onclick = ()=>{ 
