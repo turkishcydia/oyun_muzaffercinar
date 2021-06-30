82163132dc4fe12df78e398811282f3c4602dcf6
__UwU__ <82547225+turkishcydia@users.noreply.github.com>
2021-06-30 19:05:31 +0300

    Update and rename app.js to destek.js

--- destek.js
+++ destek.js
@@ -1,15 +1,18 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
     apiKey: "AIzaSyB7FpY1i-0yrMFM3PmeFlJGP_ljrPfArDU",
     authDomain: "betaweb-7e3fb.firebaseapp.com",
+    databaseURL: "https://betaweb-7e3fb-default-rtdb.firebaseio.com",
     projectId: "betaweb-7e3fb",
     storageBucket: "betaweb-7e3fb.appspot.com",
     messagingSenderId: "453663035570",
     appId: "1:453663035570:web:491c4eae35b090e472b9ab",
     measurementId: "G-30MMKLXM52"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 // Refernece contactInfo collections
 let contactInfo = firebase.database().ref("infos");
